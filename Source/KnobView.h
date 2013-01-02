f0b24b5d9cb5f8592acc7d1b42f4327eddbec37c
Oliver Greschke <ogreschke@googlemail.com>
2013-01-03 00:34:49 +0100

    changed folder structure: upload source

---
+++ Source/KnobView.h
@@ -0,0 +1,12 @@
+//
+//  KnobView.h
+//
+//  Created by Oliver Greschke on 29.12.12.
+//  Copyright (c) 2012 Oliver Greschke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KnobView : UIControl
+
+@end
